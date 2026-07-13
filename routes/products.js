544e95b7eec0f8066cb733cbdff6dd0bfb73ea82
esztyp <striveraca@gmail.com>
2026-07-13 15:48:44 -0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -9,13 +9,17 @@ const {
   registerMovement,
   getAlertas,
 } = require('../controllers/productController');
+const { protegerRuta, soloRoles } = require('../middleware/auth');
+
+// Todas las rutas de productos requieren haber iniciado sesión
+router.use(protegerRuta);
 
 router.get('/alertas/bajos', getAlertas);
 router.get('/', getProducts);
 router.get('/:id', getProduct);
 router.post('/', createProduct);
 router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.delete('/:id', soloRoles('administrador'), deleteProduct);
 router.post('/:id/movimiento', registerMovement);
 
 module.exports = router;
